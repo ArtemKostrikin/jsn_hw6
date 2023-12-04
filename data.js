331b1d30ad0531d986bc95367dcd90a96af9f58b
ArtemKostrikin <96254980+ArtemKostrikin@users.noreply.github.com>
2023-12-04 18:21:42 +0300

    /

---
+++ data.js
@@ -0,0 +1,47 @@
+{
+    "featuredItems"; [
+      {
+        "id": 1,
+        "name": "Product 1",
+        "description": "Description of product 1",
+        "price": 52.00,
+        "imageURL": "img/prodcut1.png"
+      },
+      {
+        "id": 2,
+        "name": "Product 2",
+        "description": "Description of product 2",
+        "price": 52.00,
+        "imageURL": "img/prodcut2.png"
+      },
+      {
+        "id": 3,
+        "name": "Product 3",
+        "description": "Description of product 3",
+        "price": 52.00,
+        "imageURL": "img/prodcut3.png"
+      },
+      {
+        "id": 4,
+        "name": "Product 4",
+        "description": "Description of product 4",
+        "price": 52.00,
+        "imageURL": "img/prodcut4.png"
+      },
+      {
+        "id": 5,
+        "name": "Product 5",
+        "description": "Description of product 5",
+        "price": 52.00,
+        "imageURL": "img/prodcut5.png"
+      },
+      {
+        "id": 6,
+        "name": "Product 6",
+        "description": "Description of product 6",
+        "price": 52.00,
+        "imageURL": "img/prodcut6.png"
+      },
+
+    ]
+  }
